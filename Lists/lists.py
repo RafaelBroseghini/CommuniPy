0ff5e8483e3e78a45db7ab3054b0a1357c8b23b6
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-07-12 15:49:13 -0500

    Create lists.py

---
+++ Lists/lists.py
@@ -0,0 +1,19 @@
+"""Given test_list and a word, return new list with word concatenated to every item in the list."""
+def task1(test_list, word):
+  pass
+
+"""Given test_list return the two greatest items in the list.""" 
+def task2(test_list):
+  pass
+
+"""Given test_list return count of items between bottom and top thresholds.""" 
+def task3(test_list, bottom, top):
+  pass
+
+"""Given test_list and a delimiter, return a list separated by the delimiter in reverse order."""
+def task4(test_list):
+  pass
+
+"""Given test_list convert a list of multiple integers into a single integer"""
+def task5(test_list):
+  pass
