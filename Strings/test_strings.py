a6a2d8cf27c4fb39c886777671f36521c86b3a5a
Vishnu <vishnunilambur@gmail.com>
2018-07-13 22:48:10 +0530

    updated tests

--- Strings/test_strings.py
+++ Strings/test_strings.py
@@ -9,10 +9,27 @@ class Tester(unittest.TestCase):
         self.assertEqual(task1("t h i s i s c r a z y"),
                          "y z a r c s i s i h t")
 
+    def test_task_2(self):
+        self.assertEqual(task2("discord"), "d!sc!rd")
+        self.assertEqual(task2("Its Football Season"), "!ts F!!tb!ll S!!s!n")
+        self.assertEqual(task2("This is only a test"), "Th!s !s !nly ! t!st")
+
+
     def test_task_3(self):
-    	self.assertEqual(task3("discord"), "dsod")
-    	self.assertEqual(task3("Hello World"), "Hlowrd")
-    	self.assertEqual(task3("InDiAnA JoNeS"), "IDAAJNS")
+        pass
+
+
+    def test_task_4(self):
+        self.assertEqual(task4("discord"), "dsod")
+        self.assertEqual(task4("Hello World"), "Hlowrd")
+        self.assertEqual(task4("InDiAnA JoNeS"), "IDAAJNS")
+
+
+    def test_task_5(self):
+        self.assertEqual(task5("discord", "drocsid"), True)
+        self.assertEqual(task5("abbot", "tobba"), True)
+        self.assertEqual(task5("jones", "senoj"), True)
+        self.assertEqual(task5("abba", "abba"), True)
 
 if __name__ == "__main__":
     unittest.main()
