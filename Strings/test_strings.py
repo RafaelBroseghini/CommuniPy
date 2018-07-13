b1062d3a41a7df6efd7e93be2ad8abca31910742
Vishnu <vishnunilambur@gmail.com>
2018-07-13 19:55:35 +0530

    updated tests

--- Strings/test_strings.py
+++ Strings/test_strings.py
@@ -1,12 +1,18 @@
 from strings import *
 import unittest
 
-class Tester(unittest.TestCase):
-  def test_task_1(self):
-    self.assertEqual(task1("discord"),"drocsid")
-    self.assertEqual(task1("Hello World"), "dlroW olleH")
-    self.assertEqual(task1("t h i s i s c r a z y"), "y z a r c s i s i h t")
 
+class Tester(unittest.TestCase):
+    def test_task_1(self):
+        self.assertEqual(task1("discord"), "drocsid")
+        self.assertEqual(task1("Hello World"), "dlroW olleH")
+        self.assertEqual(task1("t h i s i s c r a z y"),
+                         "y z a r c s i s i h t")
+
+    def test_task_3(self):
+    	self.assertEqual(task3("discord"), "dsod")
+    	self.assertEqual(task3("Hello World"), "Hlowrd")
+    	self.assertEqual(task3("InDiAnA JoNeS"), "IDAAJNS")
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
