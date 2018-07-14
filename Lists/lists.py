77438294b595445bbd369012b92e583118b1e104
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-07-14 10:30:38 -0500

    lists done

--- Lists/lists.py
+++ Lists/lists.py
@@ -1,19 +1,26 @@
-"""Given test_list and a word, return new list with word concatenated to every item in the list."""
+"""Given test_list and a word, return new list with 'word' concatenated to every item in the list."""
+
 def task1(test_list, word):
   pass
 
-"""Given test_list return the two greatest items in the list.""" 
+"""Given test_list, return the two greatest items in the list as a list in sorted order.
+If list only has one item. The new list must contain that item twice.""" 
+
 def task2(test_list):
   pass
 
-"""Given test_list return count of items between bottom and top thresholds.""" 
+"""Given test_list, return the count of items between bottom and top thresholds. (inclusive)""" 
+
 def task3(test_list, bottom, top):
   pass
 
 """Given test_list and a delimiter, return a list separated by the delimiter in reverse order."""
-def task4(test_list):
+
+def task4(test_list, delimiter):
   pass
 
-"""Given test_list convert a list of multiple integers into a single integer"""
+
+"""Given test_list, convert a list of multiple integers into a single integer"""
+
 def task5(test_list):
   pass
