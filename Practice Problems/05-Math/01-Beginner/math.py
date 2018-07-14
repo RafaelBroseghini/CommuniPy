20d1d5aeff735876b8e38c50c69a1e051dff6b3f
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-07-14 17:21:28 -0500

    math problems

---
+++ Practice Problems/05-Math/01-Beginner/math.py
@@ -0,0 +1,53 @@
+__author__ = "John Doe"
+
+# For purposes of practicing do not import math.
+# Assume pi = 3.14
+
+def even_or_odd(number):
+    """
+        Given a number, return if it is even or odd.
+    """
+    pass
+
+def sum_them_all(number):
+    """
+        Given a number, return the sum of all digits from
+        zero to the number (inclusive).  
+    """
+    pass
+
+def how_many_days(days):
+    """
+        Given a certain amount of days, return a string
+        in the format: 'x year(s) y month(s) and z day(s).
+
+        For the purposes of this exercise, assume a year has
+        360 days and a month has 30 days.
+    """
+    pass
+
+def whats_the_price(diameter, price):
+    """
+        Given the diameter and price of a pizza,
+        return the cost per square inch of it.
+    """
+    pass
+
+def how_much_on_taxes(price, total):
+    """
+        Given the price of a product and the total
+        on the receipt, return the exact percentage
+        paid in taxes, with two decimals.
+    """
+    pass
+
+def calories_per_day(current, goal, max_per_day):
+    """
+        Given current weith (kg), goal weight (kg), and
+        the maximum intake of calories per day (g).
+        Find the amount of days this person will need to
+        reach their goal.
+
+        Hint: check if goal < current and vice versa.
+    """
+    pass
