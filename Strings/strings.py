28f3fe0835c436d7ebf9a19b9440854e8f0d2023
Vishnu <vishnunilambur@gmail.com>
2018-07-14 11:45:32 +0530

    Updated test

--- Strings/strings.py
+++ Strings/strings.py
@@ -4,31 +4,31 @@
   Date:
 """
 
+"""Given test_string reverse the string."""
+
 def task1(test_string):
-    """Given test_string reverse the string."""
     pass
 
+"""Given test_string replace all vowels with '!'."""
 
 def task2(test_string):
-    """Given test_string replace all vowels with '!'."""
     pass
 
+"""Given test_string return a string with only distinct
+characters from test_string in the order they appear."""
 
 def task3(test_string):
-    """Given test_string return a string with only distinct
-    characters from test_string in the order they appear."""
     pass
 
+"""Given test_string return string with only elements at
+even indexes in order"""
 
 def task4(test_string):
-    """Given test_string return string with only elements at
-    even indexes in order"""
-
     pass
 
+"""Given test_string and test_string2 return True or False
+  if one is palindrome of the other. Palindrome: Same string
+  if read backwards."""
 
 def task5(test_string, test_string2):
-    """Given test_string and test_string2 return True or False
-      if one is palindrome of the other. Palindrome: Same string
-      if read backwards."""
     pass
