adfed06e935006af9eb95babaa0dff7d5a5640ca
Pedro Teles <pedroateles@gmail.com>
2018-07-14 18:02:56 -0300

    add 5 Math tasks

---
+++ Practice Problems/05-Math/01-Beginner/math.py
@@ -0,0 +1,28 @@
+""" Given test_number return 'Even' or 'Odd' """
+
+def even_or_odd_number(test_number):
+    pass
+
+""" Given test_number and test_number2 return True or False
+    if test_number times two is lower than test_number2 """
+
+def lower_than(test_number, test_number2):
+    pass
+
+""" Given test_number and test_number2 return True or False
+    if test_number times three is bigger than test_number2 """
+
+def bigger_than(test_number, test_number2):
+    pass
+
+""" Given test_number, test_number2 and test_number3 return True or False
+    if test_number plus test_number2 equals to test_number3 """
+
+def equals_to(test_number, test_number2, test_number3):
+    pass
+
+""" Given test_number, test_number2 and test_number3 return True or False
+    if test_number minus test_number2 not equals to test_number3 """
+
+def not_equals_to(test_number, test_number2, test_number3):
+    pass
