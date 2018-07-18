22153b66b02627471eb224fa9b42c2a39580c182
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-07-18 14:35:19 -0500

    Create my_first_bot.py

---
+++ Project Ideas/Make Your Own Bot/my_first_bot.py
@@ -0,0 +1,14 @@
+# IDEA: Define what kind of bot you want to create
+
+# Define how the bot will interact with an user.
+  # Will it be a conversation based interaction?
+  # Will the bot return random responses?
+
+# Use a while loop that will run indefinitely until user enters a certain keyword (e.g 'stop')
+
+"""
+user_input = input("Hello, would you like to hear a joke? ")
+while user_input != 'stop':
+  print(random_joke)
+  user_input = input("Would you like to hear another joke? ")
+"""
