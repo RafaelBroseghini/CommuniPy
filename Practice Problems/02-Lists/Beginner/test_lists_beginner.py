03e1225ae47c27aad9f886a75035430f0d414062
Pedro Teles <pedroateles@gmail.com>
2018-07-29 14:19:24 -0300

    Add in_between_bottom_and_top.py in new format

--- Practice Problems/02-Lists/Beginner/test_lists_beginner.py
+++ Practice Problems/02-Lists/Beginner/test_lists_beginner.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, TestSuite, TextTestRunner, main
 from concatenating_word import concatenating_word
 from two_greatest_items import two_greatest_items
+from in_between_bottom_and_top import in_between_bottom_and_top
 
 class ListsBeginnerTestCase(TestCase):
     def test_concatenating_word(self):
@@ -14,6 +15,12 @@ class ListsBeginnerTestCase(TestCase):
         self.assertEqual(two_greatest_items([33, 99, 12, 43, 136, 1, 4]), [99, 136])
         self.assertEqual(two_greatest_items([55]), [55, 55])
         print("\nPassed two_greatest_items with no erros!")
+
+    def test_in_between_bottom_and_top(self):
+        self.assertEqual(in_between_bottom_and_top([3, 12, 74, 6, 43], 10, 50), [12, 43])
+        self.assertEqual(in_between_bottom_and_top([-67, -89, 31, 10, 89, 125, 431], -100, 200), [-67, -89, 31, 10, 89, 125])
+        self.assertEqual(in_between_bottom_and_top([1, 5, 12, -12, 4, 8], 0, 10), [1, 5, 4, 8])
+        print("\nPassed in_between_bottom_and_top with no erros!")
 
 def test_one(test_name):
     suite = TestSuite()
