67df99058c564129edd35ea88e4f2e08fb3e6707
Pedro Teles <pedroateles@gmail.com>
2018-07-29 13:08:37 -0300

    Add two_greatest_items.py in new format

--- Practice Problems/02-Lists/Beginner/test_lists_beginner.py
+++ Practice Problems/02-Lists/Beginner/test_lists_beginner.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, TestSuite, TextTestRunner, main
 from concatenating_word import concatenating_word
+from two_greatest_items import two_greatest_items
 
 class ListsBeginnerTestCase(TestCase):
     def test_concatenating_word(self):
@@ -7,6 +8,12 @@ class ListsBeginnerTestCase(TestCase):
         self.assertEqual(concatenating_word(["Hello", "GoodEvening", "GoodBye"], "World"), ["HelloWorld", "GoodEveningWorld", "GoodByeWorld"])
         self.assertEqual(concatenating_word(["Communi", "Git", "Language"], "Py"), ["CommuniPy", "GitPy", "LanguagePy"])
         print("\nPassed concatenating_word with no errors!")
+
+    def test_two_greatest_items(self):
+        self.assertEqual(two_greatest_items([1, 2, 3, 4, 5]), [4, 5])
+        self.assertEqual(two_greatest_items([33, 99, 12, 43, 136, 1, 4]), [99, 136])
+        self.assertEqual(two_greatest_items([55]), [55, 55])
+        print("\nPassed two_greatest_items with no erros!")
 
 def test_one(test_name):
     suite = TestSuite()
---
+++ Practice Problems/02-Lists/Beginner/two_greatest_items.py
@@ -0,0 +1,30 @@
+__author__ = "John Doe"
+
+def two_greatest_items(test_list):
+    """
+	Problem Statement-
+		Given test_list return the two greatest
+        items in that list as a new list in sorted
+        order.
+
+	Input-
+	a. test_list: list
+
+	Output-
+	    The function should return a new list with the two
+        greatest items in a sorted order.
+
+	Example-
+		>>> two_greatest_items([44, 32, 7, 9])
+		[32, 44]
+
+		>>> two_greatest_items([5])
+		[5, 5]
+	"""
+    pass
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
+    from test_lists_beginner import test_one
+    test_one('test_two_greatest_items')
